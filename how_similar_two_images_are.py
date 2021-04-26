6d1f13336d05a8a1a1d7dbad746e73add418f339
sss123-bit <61051991+sss123-bit@users.noreply.github.com>
2021-04-27 05:19:00 +0530

    Add files via upload

---
+++ how_similar_two_images_are.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+def check_id(p1,p2):
+    
+    original = cv2.imread(p1)
+    image_to_compare = cv2.imread(p2)
+
+    if original.shape == image_to_compare.shape:
+        print("The images have same size and channels")
+        difference = cv2.subtract(original, image_to_compare)
+        b, g, r = cv2.split(difference)
+
+        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+            print("The images are completely Equal")
+        else:
+            print("The images are NOT equal")
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp_1, desc_1 = sift.detectAndCompute(original, None)
+    kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+
+    index_params = dict(algorithm=0, trees=5)
+    search_params = dict()
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(desc_1, desc_2, k=2)
+
+    good_points = []
+    for m, n in matches:
+        if m.distance < 0.6*n.distance:
+            good_points.append(m)
+
+    number_keypoints = 0
+    if len(kp_1) <= len(kp_2):
+        number_keypoints = len(kp_1)
+    else:
+        number_keypoints = len(kp_2)
+
+    val=len(good_points) / number_keypoints * 100
+
+    result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
+
+    cv2.imwrite("feature_matching.jpg", result)
