bbad29152a95ec98433a3ab541cf44954c567c03
sss123-bit <61051991+sss123-bit@users.noreply.github.com>
2021-05-31 14:53:04 +0530

    Add files via upload

---
+++ dd.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+c=0.5
+net = cv2.dnn.readNetFromCaffe("C:/Users/Administrator/Downloads/face-recognition-using-deep-learning-master/face-recognition-using-deep-learning-master/face_detection_model/deploy.prototxt","C:/Users/Administrator/Downloads/face-recognition-using-deep-learning-master/face-recognition-using-deep-learning-master/face_detection_model/res10_300x300_ssd_iter_140000.caffemodel ")
+image = cv2.imread("C:/Users/Administrator/Desktop/caffemodel//id1.png")
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
+	(300, 300), (104.0, 177.0, 123.0))
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+for i in range(0, detections.shape[2]):
+	confidence = detections[0, 0, i, 2]
+	if confidence > c :
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(image, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+cv2.imshow("Output", image)
+cv2.waitKey(0)
